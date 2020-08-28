31c431c9799fcc90989f45abf0ddaec8cf46486d
megashrieks <shrikanthbudya@gmail.com>
2020-08-29 00:41:15 +0530

    breaking down processing into chunks the processing of chunks is randomized for ease chunks are of square root of size so the chunks are proportional to size

--- scripts/script.js
+++ scripts/script.js
@@ -8,28 +8,54 @@ const resize = () => {
     size = Math.min(can.width, can.height);
 }
 window.onresize = resize();
-
+let divisions, divisionSize, ds, coords;
 const setup = () => {
     ctx.clearRect(0, 0, can.width, can.height);
-    idata = ctx.getImageData(can.width / 2 - size / 2, can.height / 2 - size / 2, size, size);
+
+    divisions = size**.5;
+    divisionSize = size / divisions;
+    ds = Math.ceil(divisionSize);
+    coords = [];
+    for (let x = 0; x < divisions; ++x)
+        for (let y = 0; y < divisions; ++y)
+            coords.push([x, y]);
+    for (let i = 0; i < coords.length; ++i){
+        let temp = coords[i];
+        let rand = ~~(Math.random() * coords.length);
+        coords[i] = coords[rand];
+        coords[rand] = temp;
+    }
 }
-const setColor = (arr, index, func) => {
-    let actual = index / 4;
-    const res = func(((actual % size) / size), 1-~~(actual / size) / size);
+const setColor = (arr, index, func,x,y) => {
+    // let actual = offset + index / 4;
+    const res = func((x / size), 1-~~y / size);
     arr[index] = Math.max(Math.min(res[0]*255,255),0);
     arr[index + 1] = Math.max(Math.min(res[1] * 255, 255), 0);
     arr[index + 2] = Math.max(Math.min(res[2] * 255, 255), 0);
     arr[index + 3] = 255;
 }
 
-const draw = () => {
-    for (let i = 0; i < idata.data.length; i += 4){
-        setColor(idata.data, i, shader);
+let getChunk = (divisionSize, x, y) => {
+    return ctx.getImageData(can.width / 2 - size / 2 + divisionSize*x, can.height / 2 - size / 2 + divisionSize *y, divisionSize,divisionSize);
+}
+let putChunk = (divisionSize, x, y,data) => {
+    ctx.putImageData(data, can.width / 2 - size / 2 + divisionSize * x, can.height / 2 - size / 2 + divisionSize * y);
+}
+//so I can stop through console
+let stop = false;
+const draw = async () => {
+    let func = () => {
+        if (!coords.length) return;
+        let [x, y] = coords.pop();
+        let idata = getChunk(ds, x, y);
+        for (let i = 0; i < idata.data.length; i += 4) {
+            setColor(idata.data, i, shader, x * ds + (i / 4) % ds, y * ds + (i / 4) / ds);
+            if (stop) return;
+        }
+        putChunk(ds, x, y, idata);
+        setTimeout(()=>func(),0)
     }
-    ctx.putImageData(idata, can.width / 2 - size / 2, can.height / 2 - size / 2);
-    // ctx.strokeStyle = "rgb(150,150,150)";
-    // ctx.lineWidth = 3;
-    // ctx.strokeRect(can.width / 2 - size / 2 - 1, can.height / 2 - size / 2 - 1, size + 2, size + 2);
+    setTimeout(() => func(0, 0), 0);
 }
 resize();
 setup();
